baa9ecfebc0e794044a443a449cbe9ee21cb7b3b
Jaiganesh-J <91588062+Jaiganesh-J@users.noreply.github.com>
2024-02-18 14:58:23 +0530

    Add files via upload

---
+++ 07-Omnifood-Desktop/js/script.js
@@ -0,0 +1,88 @@
+console.log("hello world");
+
+/////////// updating current year/////
+let YearEl = document.querySelector(".year");
+let currentYear = new Date().getFullYear();
+YearEl.textContent = currentYear;
+
+/////mobile navigation//////
+let btnNavEl = document.querySelector(".btn-mobile-nav");
+let mainHeader = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  mainHeader.classList.toggle("nav--open");
+});
+
+//////sticky navigation///
+
+let sectionHeroEl = document.querySelector(".section-hero");
+
+let obs = new IntersectionObserver(
+  function (entries) {
+    let ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("nav-link"))
+      mainHeader.classList.toggle("nav--open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
